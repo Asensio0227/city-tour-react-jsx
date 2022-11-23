60de942616b16dac1786f6beca097d0ff48f6ded
Asensio0227 <mtheesikhosana@gmail.com>
2022-11-23 05:09:55 +0200

    data

--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -2,34 +2,19 @@ import React, { Component } from 'react';
 import Tour from '../Tour/Tour'
 import "./TourList.scss"
 import items from "../../tourData";
-import Client from "../../contentful"
 
 
 export default class TourList extends Component {
   state = {
     tours: []
   }
-  
-  getData = async () => {
-    try {
-      let response = await Client.getEntries({
-        content_type: "cityTour",
-        order:"sys.createdAt",
-      });
-      console.log(response.items);
-      // let tours = await this.formData(response.items);
-      console.log(this.formData(response.items));
-    } catch (error) {
-      console.log(error);
-    }
-  }; 
 
   componentDidMount() { 
-    this.getData();
+    let tours = this.formatData(items);
+    this.setState({tours});
   };
 
   formatData(venom){
-    console.log(venom);
     let tempItems = venom.map((item) => {
       let id = item.sys.id;
       let image = item.images.map((image)=>image.fields.file.url)
@@ -47,19 +32,6 @@ export default class TourList extends Component {
   
   render() {
     const { tours } = this.state;
-
-    if (tours.length < 1) {
-      return (
-        <section className="section section-center">
-          <h2>no tours available</h2>
-          <button className="btn" onClick={() => {
-            this.getData()
-          }}>
-            refresh
-          </button>
-        </section>
-      )
-    }
 
     return (
       <section>
